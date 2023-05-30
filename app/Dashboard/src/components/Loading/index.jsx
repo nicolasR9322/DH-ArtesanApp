37f7eaf6c772ca36468009011a7ac9104ea58752
nicolasR9322 <nicolas.a.rodriguez93@gmail.com>
2023-05-30 01:57:16 -0300

    error al subir los archivos

--- app/Dashboard/src/components/Loading/index.jsx
+++
@@ -1,9 +0,0 @@
-import styles from "./index.module.css";
-
-export const Loading = () => {
-    return (
-        <div className={styles.spinnerContainer}>
-            <div className="spinner-border text-danger"></div>
-        </div>
-    )
-};
--- app/Dashboard/src/pages/NotFound/index.jsx
+++
@@ -1,7 +0,0 @@
-export const NotFound = () => {
-    return (
-        <div>
-            <h1>Page not found</h1>
-        </div>
-    )
-}
